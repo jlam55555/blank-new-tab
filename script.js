e9bc6b38be1dba3c925c9ade3764c91e7b9ab8c1
Jonathan Lam <jlam55555@gmail.com>
2016-05-30 16:25:05 -0400

    Fixed lowercased filenames

--- script.js
+++ script.js
@@ -3,12 +3,11 @@
   input_elem = document.querySelector("#image_url");
   String.prototype.isOneOf = function(array) {
     for(var index in array)
-      if(this == array[index])
+      if(this.trim().toLowerCase() == array[index])
         return true;
     return false;
   };
   set_page_background = image_url => {
-    image_url = image_url.trim().toLowerCase();
     document.body.style.backgroundImage = "url(" + image_url + ")";
     document.body.style.backgroundColor = image_url.isOneOf(["empty", "blank", "white", ""]) ? "white" : (image_url.isOneOf(["red", "orange", "yellow", "green", "blue", "purple", "black"]) || image_url.match(/^#[A-Fa-f0-9]{6}$/)) ? image_url : "transparent";
     input_elem.value = image_url;
