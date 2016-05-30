2da592fb866c1c20cd96246a5ab7b661577e915f
Jonathan Lam <jlam55555@gmail.com>
2016-05-30 17:13:30 -0400

    Fixed errors with empty input

--- src/script.js
+++ src/script.js
@@ -8,13 +8,15 @@
     return false;
   };
   set_page_background = image_url => {
-    document.body.style.backgroundImage = "url(" + image_url + ")";
     document.body.style.backgroundColor = image_url.isOneOf(["empty", "blank", "white", ""]) ? "white" : (image_url.isOneOf(["red", "orange", "yellow", "green", "blue", "purple", "black"]) || image_url.match(/^#[A-Fa-f0-9]{6}$/)) ? image_url : "transparent";
     input_elem.value = image_url;
+    if(image_url == "")
+      return;
+    document.body.style.backgroundImage = "url(" + image_url + ")";
   };
-  set_page_background((image_url = localStorage.getItem("image_url"))); 
+  set_page_background((image_url = localStorage.getItem("image_url") || "")); 
   input_elem.addEventListener("blur", () => {
-    set_page_background(input_elem.value);
+    set_page_background(input_elem.value || "");
     localStorage.setItem("image_url", input_elem.value);
   });
   input_elem.addEventListener("keyup", e => {
